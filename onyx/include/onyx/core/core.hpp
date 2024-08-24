3c7fa7c5f04f67003113dc20666612eb2af6a982
Ismael Bueno <ismaelwno@gmail.com>
2024-08-24 18:54:05 +0100

    Add initial onyx project source files

---
+++ onyx/include/onyx/core/core.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef KIT_OS_WINDOWS
+#    ifdef ONYX_SHARED_LIBRARY
+#        ifdef ONYX_EXPORT
+#            define ONYX_API __declspec(dllexport)
+#        else
+#            define ONYX_API __declspec(dllimport)
+#        endif
+#    else
+#        define ONYX_API
+#    endif
+#elif defined(KIT_OS_LINUX) || defined(KIT_OS_APPLE)
+#    if defined(ONYX_SHARED_LIBRARY) && defined(ONYX_EXPORT)
+#        define ONYX_API __attribute__((visibility("default")))
+#    else
+#        define ONYX_API
+#    endif
+#endif
