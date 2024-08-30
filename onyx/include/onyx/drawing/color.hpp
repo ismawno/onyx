139ebf94de573e6b38f363444c4a3b62c57ebfca
Ismael Bueno <ismaelwno@gmail.com>
2024-08-30 23:06:43 +0100

    Implement color class

---
+++ onyx/include/onyx/drawing/color.hpp
@@ -0,0 +1,123 @@
+#pragma once
+
+#include "onyx/core/alias.hpp"
+#include "onyx/core/glm.hpp"
+#include "kit/core/concepts.hpp"
+
+namespace ONYX
+{
+struct Color
+{
+    explicit Color(f32 p_Val = 1.f) noexcept;
+    explicit Color(u32 p_Val) noexcept;
+    explicit Color(u8 p_Val) noexcept;
+
+    explicit(false) Color(const vec4 &p_RGBA) noexcept;
+    explicit(false) Color(const vec3 &p_RGB, f32 p_Alpha = 1.f) noexcept;
+
+    Color(f32 p_Red, f32 p_Green, f32 p_Blue, f32 p_Alpha = 1.f) noexcept;
+    Color(u32 p_Red, u32 p_Green, u32 p_Blue, u32 p_Alpha = 255) noexcept;
+    Color(u8 p_Red, u8 p_Green, u8 p_Blue, u8 p_Alpha = 255) noexcept;
+
+    Color(const Color &p_RGB, f32 p_Alpha) noexcept;
+    Color(const Color &p_RGB, u32 p_Alpha) noexcept;
+    Color(const Color &p_RGB, u8 p_Alpha) noexcept;
+
+    union {
+        vec4 RGBA;
+        vec3 RGB;
+    };
+
+    u8 Red() const noexcept;
+    u8 Green() const noexcept;
+    u8 Blue() const noexcept;
+    u8 Alpha() const noexcept;
+
+    void Red(u8 p_Red) noexcept;
+    void Green(u8 p_Green) noexcept;
+    void Blue(u8 p_Blue) noexcept;
+    void Alpha(u8 p_Alpha) noexcept;
+
+    template <typename T> T ToHexadecimal(bool p_Alpha = true) const noexcept;
+
+    static Color FromHexadecimal(u32 p_Hex, bool p_Alpha = true) noexcept;
+    static Color FromHexadecimal(const std::string &p_Hex, bool p_Alpha = true) noexcept;
+
+    static Color FromString(std::string_view p_Color) noexcept;
+
+    const f32 *Pointer() const noexcept;
+    f32 *Pointer() noexcept;
+
+    explicit(false) operator const vec4 &() const noexcept;
+    explicit(false) operator const vec3 &() const noexcept;
+
+    Color &operator+=(const Color &p_Rhs) noexcept;
+    Color &operator-=(const Color &p_Rhs) noexcept;
+    Color &operator*=(const Color &p_Rhs) noexcept;
+    Color &operator/=(const Color &p_Rhs) noexcept;
+    template <typename T> Color &operator*=(const T &p_Rhs) noexcept
+    {
+        RGB = glm::clamp(RGB * p_Rhs, 0.f, 1.f);
+        return *this;
+    }
+    template <typename T> Color &operator/=(const T &p_Rhs) noexcept
+    {
+        RGB = glm::clamp(RGB / p_Rhs, 0.f, 1.f);
+        return *this;
+    }
+
+    friend Color operator+(const Color &lhs, const Color &rhs)
+    {
+        Color result = lhs;
+        result += rhs;
+        return result;
+    }
+
+    friend Color operator-(const Color &lhs, const Color &rhs)
+    {
+        Color result = lhs;
+        result -= rhs;
+        return result;
+    }
+
+    friend Color operator*(const Color &lhs, const Color &rhs)
+    {
+        Color result = lhs;
+        result *= rhs;
+        return result;
+    }
+
+    friend Color operator/(const Color &lhs, const Color &rhs)
+    {
+        Color result = lhs;
+        result /= rhs;
+        return result;
+    }
+
+    friend bool operator==(const Color &lhs, const Color &rhs)
+    {
+        return lhs.RGBA == rhs.RGBA;
+    }
+    friend bool operator!=(const Color &lhs, const Color &rhs)
+    {
+        return lhs.RGBA != rhs.RGBA;
+    }
+
+    static const Color RED;
+    static const Color GREEN;
+    static const Color BLUE;
+    static const Color MAGENTA;
+    static const Color CYAN;
+    static const Color ORANGE;
+    static const Color YELLOW;
+    static const Color BLACK;
+    static const Color PINK;
+    static const Color PURPLE;
+    static const Color WHITE;
+    static const Color TRANSPARENT;
+
+  private:
+    // This will be useful for serialization (not implemented yet)
+    static const HashMap<std::string, Color> s_ColorMap;
+};
+} // namespace ONYX
---
+++ onyx/src/drawing/color.cpp
@@ -0,0 +1,221 @@
+#include "core/pch.hpp"
+#include "onyx/drawing/color.hpp"
+#include "kit/core/logging.hpp"
+#include <sstream>
+
+namespace ONYX
+{
+static u8 toInt(const f32 p_Val)
+{
+    return static_cast<u8>(p_Val * 255.f);
+}
+
+static f32 toFloat(const u8 p_Val)
+{
+    constexpr f32 oneOver255 = 1.f / 255.f;
+    return static_cast<f32>(p_Val) * oneOver255;
+}
+static f32 toFloat(const u32 p_Val)
+{
+    constexpr f32 oneOver255 = 1.f / 255.f;
+    return static_cast<f32>(p_Val) * oneOver255;
+}
+
+Color::Color(const f32 p_Val) noexcept : Color(p_Val, p_Val, p_Val, 1.f)
+{
+    KIT_ASSERT(p_Val <= 1.f && p_Val >= 0.f, "Color floating values must be in the range [0, 1]");
+}
+Color::Color(const u32 p_Val) noexcept : Color(p_Val, p_Val, p_Val, 255u)
+{
+}
+Color::Color(const u8 p_Val) noexcept : Color(p_Val, p_Val, p_Val, 255)
+{
+}
+
+Color::Color(const vec4 &p_RGBA) noexcept : RGBA(p_RGBA)
+{
+    KIT_ASSERT(p_RGBA.r <= 1.f && p_RGBA.r >= 0.f, "Red value must be in the range [0, 1]");
+    KIT_ASSERT(p_RGBA.g <= 1.f && p_RGBA.g >= 0.f, "Green value must be in the range [0, 1]");
+    KIT_ASSERT(p_RGBA.b <= 1.f && p_RGBA.b >= 0.f, "Blue value must be in the range [0, 1]");
+    KIT_ASSERT(p_RGBA.a <= 1.f && p_RGBA.a >= 0.f, "Alpha value must be in the range [0, 1]");
+}
+Color::Color(const vec3 &p_RGB, const f32 p_Alpha) noexcept : RGBA(p_RGB, p_Alpha)
+{
+    KIT_ASSERT(p_RGB.r <= 1.f && p_RGB.r >= 0.f, "Red value must be in the range [0, 1]");
+    KIT_ASSERT(p_RGB.g <= 1.f && p_RGB.g >= 0.f, "Green value must be in the range [0, 1]");
+    KIT_ASSERT(p_RGB.b <= 1.f && p_RGB.b >= 0.f, "Blue value must be in the range [0, 1]");
+}
+
+Color::Color(const f32 r, const f32 g, const f32 b, const f32 a) noexcept : RGBA(r, g, b, a)
+{
+    KIT_ASSERT(r <= 1.f && r >= 0.f, "Red value must be in the range [0, 1]");
+    KIT_ASSERT(g <= 1.f && g >= 0.f, "Green value must be in the range [0, 1]");
+    KIT_ASSERT(b <= 1.f && b >= 0.f, "Blue value must be in the range [0, 1]");
+    KIT_ASSERT(a <= 1.f && a >= 0.f, "Alpha value must be in the range [0, 1]");
+}
+Color::Color(const u32 r, const u32 g, const u32 b, const u32 a) noexcept
+    : RGBA(toFloat(r), toFloat(g), toFloat(b), toFloat(a))
+{
+    KIT_ASSERT(r < 256, "Red value must be in the range [0, 255]");
+    KIT_ASSERT(g < 256, "Green value must be in the range [0, 255]");
+    KIT_ASSERT(b < 256, "Blue value must be in the range [0, 255]");
+    KIT_ASSERT(a < 256, "Alpha value must be in the range [0, 255]");
+}
+Color::Color(const u8 r, const u8 g, const u8 b, const u8 a) noexcept
+    : RGBA(toFloat(r), toFloat(g), toFloat(b), toFloat(a))
+{
+    // No asserts needed: u8 is always in the range [0, 255]
+}
+
+Color::Color(const Color &p_RGB, const f32 p_Alpha) noexcept : RGBA(vec3(p_RGB.RGBA), p_Alpha)
+{
+    KIT_ASSERT(p_Alpha <= 1.f && p_Alpha >= 0.f, "Alpha value must be in the range [0, 1]");
+}
+
+Color::Color(const Color &p_RGB, const u32 p_Alpha) noexcept : RGBA(vec3(p_RGB.RGBA), toFloat(p_Alpha))
+{
+    KIT_ASSERT(p_Alpha < 256, "Alpha value must be in the range [0, 255]");
+}
+Color::Color(const Color &p_RGB, const u8 p_Alpha) noexcept : RGBA(vec3(p_RGB.RGBA), toFloat(p_Alpha))
+{
+    // No asserts needed: u8 is always in the range [0, 255]
+}
+
+u8 Color::Red() const noexcept
+{
+    return toInt(RGBA.r);
+}
+u8 Color::Green() const noexcept
+{
+    return toInt(RGBA.g);
+}
+u8 Color::Blue() const noexcept
+{
+    return toInt(RGBA.b);
+}
+u8 Color::Alpha() const noexcept
+{
+    return toInt(RGBA.a);
+}
+
+void Color::Red(u8 r) noexcept
+{
+    RGBA.r = toFloat(r);
+}
+void Color::Green(u8 g) noexcept
+{
+    RGBA.g = toFloat(g);
+}
+void Color::Blue(u8 b) noexcept
+{
+    RGBA.b = toFloat(b);
+}
+void Color::Alpha(u8 a) noexcept
+{
+    RGBA.a = toFloat(a);
+}
+
+template <> u32 Color::ToHexadecimal<u32>(const bool p_Alpha) const noexcept
+{
+    if (p_Alpha)
+        return Red() << 24 | Green() << 16 | Blue() << 8 | Alpha();
+    return Red() << 16 | Green() << 8 | Blue();
+}
+
+template <> std::string Color::ToHexadecimal<std::string>(const bool p_Alpha) const noexcept
+{
+    std::stringstream ss;
+    ss << std::hex << ToHexadecimal<u32>(p_Alpha);
+    std::string hex = ss.str();
+    if (p_Alpha)
+        while (hex.size() < 8)
+            hex = "0" + hex;
+    else
+        while (hex.size() < 6)
+            hex = "0" + hex;
+    return hex;
+}
+
+Color Color::FromHexadecimal(const u32 p_Hex, const bool p_Alpha) noexcept
+{
+    if (p_Alpha)
+        return {p_Hex >> 24, (p_Hex >> 16) & 0xFF, (p_Hex >> 8) & 0xFF, p_Hex & 0xFF};
+    return {p_Hex >> 16, (p_Hex >> 8) & 0xFF, p_Hex & 0xFF};
+}
+Color Color::FromHexadecimal(const std::string &p_Hex, const bool p_Alpha) noexcept
+{
+    u32 val;
+    std::stringstream ss;
+    ss << std::hex << p_Hex;
+    ss >> val;
+    return FromHexadecimal(val, p_Alpha);
+}
+
+Color Color::FromString(const std::string_view p_Color) noexcept
+{
+    const auto it = s_ColorMap.find(p_Color);
+    if (it != s_ColorMap.end())
+        return it->second;
+    return Color::WHITE;
+}
+
+const f32 *Color::Pointer() const noexcept
+{
+    return glm::value_ptr(RGBA);
+}
+f32 *Color::Pointer() noexcept
+{
+    return glm::value_ptr(RGBA);
+}
+
+Color::operator const vec4 &() const noexcept
+{
+    return RGBA;
+}
+Color::operator const vec3 &() const noexcept
+{
+    return RGB;
+}
+
+Color &Color::operator+=(const Color &rhs) noexcept
+{
+    RGB = glm::clamp(RGB + rhs.RGB, 0.f, 1.f);
+    return *this;
+}
+Color &Color::operator-=(const Color &rhs) noexcept
+{
+    RGB = glm::clamp(RGB - rhs.RGB, 0.f, 1.f);
+    return *this;
+}
+
+Color &Color::operator*=(const Color &rhs) noexcept
+{
+    RGB = glm::clamp(RGB * rhs.RGB, 0.f, 1.f);
+    return *this;
+}
+
+Color &Color::operator/=(const Color &rhs) noexcept
+{
+    RGB = glm::clamp(RGB / rhs.RGB, 0.f, 1.f);
+    return *this;
+}
+
+const Color Color::RED{255u, 0u, 0u};
+const Color Color::GREEN{0u, 255u, 0u};
+const Color Color::BLUE{0u, 0u, 255u};
+const Color Color::MAGENTA{255u, 0u, 255u};
+const Color Color::CYAN{0u, 255u, 255u};
+const Color Color::ORANGE{255u, 165u, 0u};
+const Color Color::YELLOW{255u, 255u, 0u};
+const Color Color::BLACK{0u};
+const Color Color::PINK{255u, 192u, 203u};
+const Color Color::PURPLE{191u, 64u, 191u};
+const Color Color::WHITE{255u};
+const Color Color::TRANSPARENT{WHITE, 0u};
+
+const HashMap<std::string, Color> Color::s_ColorMap{
+    {"red", RED},   {"green", GREEN},   {"blue", BLUE},     {"magenta", MAGENTA},
+    {"cyan", CYAN}, {"orange", ORANGE}, {"yellow", YELLOW}, {"black", BLACK},
+    {"pink", PINK}, {"purple", PURPLE}, {"white", WHITE},   {"transparent", TRANSPARENT}};
+
+} // namespace ONYX
