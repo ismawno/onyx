c024984b50b69dbe4f10048b670eb44710e2e2a4
Ismael Bueno <ismaelwno@gmail.com>
2024-11-11 01:06:26 +0100

    Ensure stencil buffer is cleared at the beginning of a render pass

--- onyx/onyx/rendering/swap_chain.cpp
+++ onyx/onyx/rendering/swap_chain.cpp
@@ -258,7 +258,7 @@ void SwapChain::createRenderPass() noexcept
     depthAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
     depthAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
     depthAttachment.storeOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    depthAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+    depthAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
     depthAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
     depthAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
     depthAttachment.finalLayout = VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL;
