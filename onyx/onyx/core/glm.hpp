e1e4b7dc2cc78c196bf283fbbaf6316b5b2170a7
Ismael Bueno <ismaelwno@gmail.com>
2024-12-29 03:09:09 +0000

    Create glm aliases from template

--- onyx/onyx/core/glm.hpp
+++ onyx/onyx/core/glm.hpp
@@ -6,15 +6,15 @@
 
 namespace Onyx
 {
-using vec2 = glm::vec2;
-using vec3 = glm::vec3;
-using vec4 = glm::vec4;
+using vec2 = glm::vec<2, f32>;
+using vec3 = glm::vec<3, f32>;
+using vec4 = glm::vec<4, f32>;
 
 template <Dimension D> using vec = glm::vec<D, f32>;
 
-using mat2 = glm::mat2;
-using mat3 = glm::mat3;
-using mat4 = glm::mat4;
+using mat2 = glm::mat<2, 2, f32>;
+using mat3 = glm::mat<3, 3, f32>;
+using mat4 = glm::mat<4, 4, f32>;
 
 template <Dimension D> using mat = glm::mat<D + 1, D + 1, f32>;
 
