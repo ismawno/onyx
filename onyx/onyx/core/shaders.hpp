0cf5a92f1966d160852d2eaed798821b00326549
Ismael Bueno <ismaelwno@gmail.com>
2025-01-01 18:08:27 +0000

    Add some documentation regarding shader functions

--- onyx/onyx/core/shaders.hpp
+++ onyx/onyx/core/shaders.hpp
@@ -17,6 +17,27 @@ template <Dimension D, DrawMode DMode> struct ONYX_API Shaders
 
 namespace Onyx
 {
+/**
+ * @brief This is a high level function to create a compiled shader binary from a source glsl shader file.
+ *
+ * First, the function checks if there exists an up-to-date binary file for the given source file at
+ * <source-parent>/bin/<shader-filename>.spv. If it does, the function returns the shader module created from that
+ * existing binary file. If it doesn't, the function compiles the source file into a binary file using the glslc
+ * executable (its path stored in the VKIT_GLSL_BINARY macro), located at <source-parent>/bin/<shader-filename>.spv
+ * and returns the shader module created from the binary file.
+ *
+ * @param p_SourcePath The path to the source file.
+ * @return VKit::Shader The created shader.
+ */
 ONYX_API VKit::Shader CreateShader(std::string_view p_SourcePath) noexcept;
+
+/**
+ * @brief Get a full pass vertex shader.
+ *
+ * This function returns an already available full pass vertex shader that outputs the UV coordinates of the whole
+ * screen to the fragment shader.
+ *
+ * @return const VKit::Shader& The full pass vertex shader.
+ */
 ONYX_API const VKit::Shader &GetFullPassVertexShader() noexcept;
 } // namespace Onyx
