07783d370ffab37d88d082cb9feadd6352cea126
Ismael Bueno <ismaelwno@gmail.com>
2025-11-02 18:08:39 +0100

    feat: Add a flake.nix file for NixOS compatibility

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Dev shell for onyx";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "onyx";
+
+        buildInputs = with pkgs; [
+          cmake
+          clang-tools
+          clang
+          lld
+          llvmPackages_19.llvm
+          linuxPackages.perf
+          fmt
+          hwloc
+          pkg-config
+          wayland
+          wayland-protocols
+          wayland-scanner
+          fontconfig
+          expat
+          glm
+          glfw
+          libxkbcommon
+          libffi
+          python313
+          vulkan-headers
+          vulkan-loader
+          vulkan-tools
+          vulkan-memory-allocator
+          spirv-tools
+          shaderc
+        ];
+        shellHook = ''
+          export SHELL=${pkgs.zsh}/bin/zsh
+          export LD_LIBRARY_PATH=${pkgs.vulkan-loader}/lib:$LD_LIBRARY_PATH
+          export CC=clang
+          export CXX=clang++
+        '';
+      };
+    };
+}
