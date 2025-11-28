6547da55ece22ef934d90d9dcd29d5eb115af225
Ismael Bueno <ismaelwno@gmail.com>
2025-11-28 20:43:34 +0100

    fix: Supress -Wnontrivial-memcall only if not on macos

--- onyx/onyx/core/imgui.hpp
+++ onyx/onyx/core/imgui.hpp
@@ -1,7 +1,9 @@
 #include "tkit/preprocessor/system.hpp"
 TKIT_COMPILER_WARNING_IGNORE_PUSH()
+#ifndef TKIT_OS_APPLE
 TKIT_CLANG_WARNING_IGNORE("-Wnontrivial-memcall")
 TKIT_GCC_WARNING_IGNORE("-Wnontrivial-memcall")
+#endif
 #ifdef ONYX_ENABLE_IMGUI
 #    include <imgui.h>
 #    include <backends/imgui_impl_glfw.h>
