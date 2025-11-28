c52a092388593c97900a12b446f67e73403c655c
Ismael Bueno <ismaelwno@gmail.com>
2025-11-28 20:47:25 +0100

    refactor: Remove now unnecessary warning supression

--- onyx/onyx/core/imgui.hpp
+++ onyx/onyx/core/imgui.hpp
@@ -1,12 +1,5 @@
-#include "tkit/preprocessor/system.hpp"
-TKIT_COMPILER_WARNING_IGNORE_PUSH()
-#ifndef TKIT_OS_APPLE
-TKIT_CLANG_WARNING_IGNORE("-Wnontrivial-memcall")
-TKIT_GCC_WARNING_IGNORE("-Wnontrivial-memcall")
-#endif
 #ifdef ONYX_ENABLE_IMGUI
 #    include <imgui.h>
 #    include <backends/imgui_impl_glfw.h>
 #    include <backends/imgui_impl_vulkan.h>
 #endif
-TKIT_COMPILER_WARNING_IGNORE_POP()
