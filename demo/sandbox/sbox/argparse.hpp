1754ebe5befe87e0a337ce14518fe8f89dada618
Ismael Bueno <ismaelwno@gmail.com>
2026-02-19 13:21:25 +0100

    chore: Remove previous sandbox

--- demo/sandbox/sbox/argparse.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "onyx/core/dimension.hpp"
-#include "onyx/core/alias.hpp"
-
-namespace Onyx::Demo
-{
-Dimension ParseArguments(int argc, char **argv);
-} // namespace Onyx::Demo
